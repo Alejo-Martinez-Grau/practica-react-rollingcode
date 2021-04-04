dd00d40075795b013ea5c208aa2d96839af751b1
Alejo-AmeliaPC <alejomartinezgrau@gmail.com>
2021-04-03 22:32:27 -0300

    Ejercicio 4 original visto en clase, borra tareas por nombre

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
+import Subtitulo from "./components/Subtitulo";
+import Titulo from "./components/Titulo";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormTarea from "./components/FormTarea";
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld text="my friend."></HelloWorld>
+
+<Titulo/>
+      <Subtitulo comision='3A' estado={true}></Subtitulo>
+      <FormTarea></FormTarea>
+    
+      //<HelloWorld text="my friend."></HelloWorld>
     </div>
   );
 }
---
+++ src/components/Lista.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemTarea from "./ItemTarea";
+
+const Lista = (props) => {
+  return (
+    <ul className="list-group">
+        {
+            props.arregloTareas.map((dato,indice)=><ItemTarea nombreTarea={dato} key={indice} borrarTarea={props.borrarTarea}></ItemTarea>)
+        }
+    </ul>
+  );
+};
+
+export default Lista;
